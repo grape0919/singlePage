bbad08e13ec1d1f73c610b42f0e0cb4f07ae71e5
server <ghdry2563@gmail.com>
2021-02-02 07:23:14 +0000

    pass 에러 수정

--- app.py
+++ app.py
@@ -216,7 +216,7 @@ def init_db():
 
 
 if __name__ == '__main__':
-     
-     url = 'http://localhost'
+    pass
+    # url = 'http://localhost'
     #  webbrowser.open(url)
-     app.run(host='0.0.0.0', port=80, debug=True)
+    # app.run(host='0.0.0.0', port=80, debug=True)
